598c611b24a787749381dd083f68441b6f8f2329
jrphub <jyotiranjanpattnaik@gmail.com>
2020-02-08 08:48:25 +0530

    Java8 implementation

---
+++ src/main/java/com/tutorial/spark/core/WordCountLocalwJava8.java
@@ -0,0 +1,66 @@
+package com.tutorial.spark.core;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class WordCountLocalwJava8 {
+    public static void main(String[] args) {
+        String appName="WordCountLocalwJava8";
+        SparkConf sparkConf = new SparkConf().setAppName(appName)
+                .set("spark.executor.instances", "2")
+                .setMaster("local[*]");
+
+        //if you are connecting with hdfs
+		/*System.setProperty("HADOOP_USER_NAME", "huser");
+		System.setProperty("HADOOP_CONF_DIR","/home/jrp/softwares/hadoop-2.7.3/etc/hadoop");
+		*/
+
+
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
+        //1. get input
+        JavaRDD<String> distFile = jsc
+                .textFile(Objects.requireNonNull(
+                        WordCountLocalwJava8.class.getClassLoader().getResource("words.txt")
+                        ).getPath()
+                );
+
+        //2. Get collection of all words
+        //apple orange grapes apple orange
+        JavaRDD<String> flat_words = distFile
+                .flatMap((FlatMapFunction<String, String>) line -> Arrays.asList(line.split(" ")).iterator());
+
+        //3.
+        // apple 1
+        // orange 1
+        // grapes 1
+        // apple 1
+        // orange 1
+        JavaPairRDD<String, Long> flat_words_mapped = flat_words
+                .mapToPair((PairFunction<String, String, Long>) flat_word -> new Tuple2<>(flat_word, 1L));
+
+        //4.
+        // apple 2
+        // orange 2
+        // grapes 1
+        JavaPairRDD<String, Long> flat_words_reduced = flat_words_mapped
+                .reduceByKey((Function2<Long, Long, Long>) Long::sum);
+
+        // All spark job needs file:// or hdfs:// prefix to distinguish between
+        // local and cluster
+        flat_words_reduced
+                .saveAsTextFile("output/"+appName+"_" + System.currentTimeMillis());
+
+        jsc.close();
+
+    }
+}
