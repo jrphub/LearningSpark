1d0e0f79b45728aa9ed6172c809fc8956fe78bdb
jrphub <jyotiranjanpattnaik@gmail.com>
2020-02-08 14:25:08 +0530

    SubmitApp added

--- src/main/java/com/tutorial/spark/core/WordCountSubmitApp.java
+++ src/main/java/com/tutorial/spark/core/WordCountSubmitApp.java
@@ -10,34 +10,38 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Objects;
 
-public class WordCountLocal {
+public class WordCountSubmitApp {
     public static void main(String[] args) {
-        SparkConf sparkConf = new SparkConf().setAppName("WordCountLocal")
-                .set("spark.executor.instances", "2")
-                .setMaster("local[*]");
+        String appName="WordCountSubmitApp";
+        if (args.length != 2) {
+            System.exit(-1);
+        }
+        String inputFile=args[0];
+        String outputDir=args[1];
+
+        SparkConf sparkConf = new SparkConf().setAppName(appName)
+                 .set("spark.executor.instances", "2");
+
+        //to run as different user which is a hadoop user, if needed
+        System.setProperty("HADOOP_USER_NAME", "huser");
 
         //if you are connecting with hdfs
-		/*System.setProperty("HADOOP_USER_NAME", "huser");
+		/*
 		System.setProperty("HADOOP_CONF_DIR","/home/jrp/softwares/hadoop-2.7.3/etc/hadoop");
 		*/
-
 
         JavaSparkContext jsc = new JavaSparkContext(sparkConf);
 
         //1. get input
         JavaRDD<String> distFile = jsc
-                .textFile(WordCountLocal.class.getClassLoader().getResource("words.txt").getPath());
+                .textFile(inputFile);
 
         //2. Get collection of all words
         //apple orange grapes apple orange
         JavaRDD<String> flat_words = distFile
-                .flatMap(new FlatMapFunction<String, String>() {
-                    public Iterator<String> call(String line) throws Exception {
-                        return Arrays.asList(line.split(" ")).iterator();
-                    }
-                });
+                .flatMap((FlatMapFunction<String, String>) line -> Arrays.asList(line.split(" ")).iterator());
 
         //3.
         // apple 1
@@ -46,32 +50,24 @@ public class WordCountLocal {
         // apple 1
         // orange 1
         JavaPairRDD<String, Long> flat_words_mapped = flat_words
-                .mapToPair(new PairFunction<String, String, Long>() {
-                    public Tuple2<String, Long> call(String flat_word)
-                            throws Exception {
-                        return new Tuple2<String, Long>(flat_word, 1L);
-                    }
-                });
+                .mapToPair((PairFunction<String, String, Long>) flat_word -> new Tuple2<>(flat_word, 1L));
 
         //4.
         // apple 2
         // orange 2
         // grapes 1
         JavaPairRDD<String, Long> flat_words_reduced = flat_words_mapped
-                .reduceByKey(new Function2<Long, Long, Long>() {
-                    public Long call(Long l1, Long l2) throws Exception {
-                        return l1 + l2;
-                    }
-                });
+                .reduceByKey((Function2<Long, Long, Long>) Long::sum);
+
+        // Delete output Directory if exists
+        // hadoop fs -rm -r /user/huser/wordcountHdfs/output
 
         // All spark job needs file:// or hdfs:// prefix to distinguish between
         // local and cluster
         flat_words_reduced
-                .saveAsTextFile("output/WordCountLocal_" + System.currentTimeMillis());
-
+                .saveAsTextFile(outputDir+"_"+System.currentTimeMillis());
 
         jsc.close();
-
 
     }
 }
